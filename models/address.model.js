ad2c53d741f1414e2a711931ad92c96f2b71ef12
Vaibhav Panwar <p.vaibhav7499@gmail.com>
2023-05-10 23:11:57 +0530

    abc

--- models/address.model.js
+++ models/address.model.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const addressSchema = mongoose.Schema({
     address: { type: String, required: [true, 'add your address'] },
     pincode: { type: Number, required: [true, 'add pincode'] },
-    type: String,
     userID: { type: String, required: true }
 })
 
