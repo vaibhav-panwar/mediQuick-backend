86b60cd408b8859035d01193c8fc8ac6c49d56f1
Vaibhav Panwar <p.vaibhav7499@gmail.com>
2023-05-12 18:55:23 +0530

    sn

--- middleware/uniqueproduct.js
+++ middleware/uniqueproduct.js
@@ -2,9 +2,9 @@ const { CartModel } = require("../models/cart.model");
 
 
 const uniquecart = async (req, res, next) => {
-    let { productId } = req.body;
+    let { productId, userID } = req.body;
     if (req.url == "/add") {
-        let user = await CartModel.findOne({ productId });
+        let user = await CartModel.findOne({ productId , userID});
         if (user) {
             res.status(200).send({ "msg": "this product already exist" })
         }
